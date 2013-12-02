97b0aba063c492c5c374fe704991fd18ba4c2d2d
Fikri Kabakcha <fkabakcha@gmail.com>
2013-12-02 23:20:11 +0100

    Remove some duplicate code

--- test/parser/HeaderParserTest.java
+++ test/parser/HeaderParserTest.java
@@ -1,63 +1,58 @@
 package parser;
+
 import static org.junit.Assert.*;
 
 import java.util.List;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import domain.Sensor;
 import domain.Unit;
 
 public class HeaderParserTest {
+    
+    private HeaderParser parser;
 
-    // TODO: remove duplication:
-    // HeaderParser parser = new HeaderParser();
+    @Before
+    public void setUp() {
+        this.parser = new HeaderParser();
+    }
 
     @Test(expected = IllegalArgumentException.class)
     public void emptyHeaderIsRejected() {
-        HeaderParser parser = new HeaderParser();
         parser.parse(null);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void emptyStringIsRejected() {
-        HeaderParser parser = new HeaderParser();
         parser.parse(new String[] {});
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void headerLessThanThreeLinesIsRejected() {
-        HeaderParser parser = new HeaderParser();
         parser.parse(new String[] { "", "" });
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void illegalUnitIsRejected() throws Exception {
-        HeaderParser parser = new HeaderParser();
-        parser.parse(new String[] {
-                "Datum;Uhrzeit;Sensor-0", 
-                "Datum;Uhrzeit;Sensor 0",
-                "Datum;Uhrzeit;UnsupportedUnit" });
+        parser.parse(new String[] { "Datum;Uhrzeit;Sensor-0",
+                "Datum;Uhrzeit;Sensor 0", "Datum;Uhrzeit;UnsupportedUnit" });
     }
 
     @Test
     public void headerWithoutSensorDefinitionsIsParsedToEmptyList()
             throws Exception {
-        HeaderParser parser = new HeaderParser();
-        List<Sensor> sensors = parser.parse(new String[] { 
-                "Datum;Uhrzeit",
-                "Datum;Uhrzeit", 
-                "Datum;Uhrzeit" });
+        List<Sensor> sensors = parser.parse(new String[] { "Datum;Uhrzeit",
+                "Datum;Uhrzeit", "Datum;Uhrzeit" });
         assertTrue(sensors.isEmpty());
     }
 
     @Test
-    public void headerWithOneSensorDefinitionsIsParsedCorrectly()
+    public void headerWithOneSensorDefinitionIsParsedCorrectly()
             throws Exception {
-        HeaderParser parser = new HeaderParser();
         List<Sensor> sensors = parser.parse(new String[] {
-                "Datum;Uhrzeit;Sensor-0", 
-                "Datum;Uhrzeit;Sensor 0",
+                "Datum;Uhrzeit;Sensor-0", "Datum;Uhrzeit;Sensor 0",
                 "Datum;Uhrzeit;kJ" });
         assertEquals(1, sensors.size());
         assertEquals("Sensor-0", sensors.get(0).getId());
@@ -68,11 +63,9 @@ public class HeaderParserTest {
     @Test
     public void headerWithTwoSensorDefinitionsIsParsedCorrectly()
             throws Exception {
-        HeaderParser parser = new HeaderParser();
         List<Sensor> sensors = parser.parse(new String[] {
                 "Datum;Uhrzeit;Sensor-0;Sensor-1",
-                "Datum;Uhrzeit;Sensor 0;Sensor 1", 
-                "Datum;Uhrzeit;kJ;mm" });
+                "Datum;Uhrzeit;Sensor 0;Sensor 1", "Datum;Uhrzeit;kJ;mm" });
         assertEquals(2, sensors.size());
         assertEquals("Sensor-1", sensors.get(1).getId());
         assertEquals("Sensor 1", sensors.get(1).getName());
